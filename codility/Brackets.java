0dce18533e932531f804dad4662ab54ef3bb04a1
Mikolaj <mikolajbibro@gmail.com>
2020-03-27 14:17:28 +0100

    init

---
+++ codility/Brackets.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Brackets {
+
+
+    public int solution(String S) {
+        Stack<Character> openBrackets = new Stack<>();
+
+        Map<Character, Character> openToCloseBracketMap = new HashMap<Character, Character>() {{
+            put('{', '}');
+            put('(', ')');
+            put('[', ']');
+        }};
+
+
+        for (int i = 0; i < S.length(); i++) {
+            Character bracket = S.charAt(i);
+
+            if (openToCloseBracketMap.get(bracket) != null) {
+                openBrackets.push(bracket);
+            } else if (openBrackets.isEmpty() || openToCloseBracketMap.get(openBrackets.pop()) != bracket) {
+                return 0;
+            }
+        }
+
+        if (!openBrackets.isEmpty()) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+}
